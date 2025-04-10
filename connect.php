bbfbc8144d6972bd76fef0a5ae9ca62a67858672
LuDaddy1509 <luhocnhan@gmail.com>
2025-04-10 18:40:42 +0700

    web

--- connect.php
+++ connect.php
@@ -1,22 +1,8 @@
 <?php
-<<<<<<< HEAD
-    $server = 'localhost';    // Sửa "sever" thành "server"
-    $user = 'root';
-    $pass = '';
-    $database = 'webbandoan2';
-    
-    $conn = new mysqli($server, $user, $pass, $database);
-    
-    if ($conn->connect_error) {
-        die('Kết nối thất bại: ' . $conn->connect_error);
-    }
-    
-    $conn->set_charset('utf8');
-=======
     $sever='localhost';
     $user ='root';
     $pass ='';
-    $database='webbandoan24';
+    $database='webbandoan2';
     $conn = new mysqLi($sever, $user, $pass, $database);
     // if($conn){
     //     mysqLi_query($conn," SET NAMES 'utf8' ");
@@ -25,5 +11,4 @@
     // else {
     //     echo 'kết nối thất bại!';
     // }
->>>>>>> 85781ed038db2eea7019327e8eaf5cd91a3614c1
 ?>
